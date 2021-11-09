b77fd5772c472bb237c6d4545c0c6c9543d12e48
Gauravself <52067262+Gauravself@users.noreply.github.com>
2021-11-09 10:49:42 +0530

    Hidden class added

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const score0P1 = document.querySelector("#score--0");
 //select ID by getelement
 const score1P2 = document.getElementById("score--1");
+const diceImg = document.querySelector(".dice");
 score0P1.textContent = 0;
 score1P2.textContent = 0;
 //Hidding dice image
-const diceImg = document.querySelector(".dice");
-diceImg.classlist.add('hidden');
+diceImg.classList.add('hidden');
