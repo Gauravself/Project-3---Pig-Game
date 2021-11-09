f736f88b6ea0ef62dcb181e5dbce80d42d861cb3
Gauravself <52067262+Gauravself@users.noreply.github.com>
2021-11-09 10:41:42 +0530

    Hidding Dice image

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 //Selecting ID by querselector
 const score0P1 = document.querySelector("#score--0");
 //select ID by getelement
-const score0P1 = document.getElementById("score--1");
+const score1P2 = document.getElementById("score--1");
+score0P1.textContent = 0;
+score1P2.textContent = 0;
+
+dice.classlist.add("hidden");
