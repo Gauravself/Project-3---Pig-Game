a584f6b7fc7a1ebe6788728f770fad6c855a0d82
Gauravself <52067262+Gauravself@users.noreply.github.com>
2021-11-09 10:32:28 +0530

    Manipulating Scores Using Dom

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+//Selecting ID by querselector
+const score0P1 = document.querySelector("#score--0");
+//select ID by getelement
+const score0P1 = document.getElementById("score--1");
