657fd989ce7e005ca35ae4b7bcf77939cc4e94e1
Gauravself <52067262+Gauravself@users.noreply.github.com>
2021-11-09 10:43:23 +0530

    a

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ const score0P1 = document.querySelector("#score--0");
 const score1P2 = document.getElementById("score--1");
 score0P1.textContent = 0;
 score1P2.textContent = 0;
-
-dice.classlist.add("hidden");
+//Hidding dice image
+const diceImg = document.querySelector(".dice");
+diceImg.classlist.add('hidden');
